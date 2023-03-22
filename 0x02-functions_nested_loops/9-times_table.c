47946263e8ba921981978081c74c9e05886b8e7e
kiemaBruce <brucekiema292@gmail.com>
2023-03-22 12:06:24 +0000

    Print the 9 times table, starting with 0
    Correct code to conform to Betty standard, correct output to match required
    output.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,76 @@
 #include "main.h"
 
+/**
+  * times_table - prints the 9 times table, starting with 0
+  */
 void times_table(void)
 {
 	int i = 0;
 	int n = 0;
+	int res = 0;
 
 	while (n < 10)
 	{
 		while (i < 10)
 		{
-			int res = n * i;
-			printf("%d", res);
-			if (i != 9){
-				printf(", ");
+			if (i != 0)
+			{
+				res = n * i;
+				/* if (i != 9) */
+				/*{*/
+					if (res <= 9)
+					{
+						printf(",  ");
+					} else if (res >= 10)
+					{
+						printf(", ");
+					}
+				/*}*/
+				printf("%d", res);
+			} else 
+			{
+				res = n * i;
+				printf("%d", res);
 			}
+			/* res = n * i; */
+
+			/* printf("%d", res); */
+			/* checker = 1; */
+
+			/* if (res <= 9 && res != 0)
+			{
+				printf(",  ");
+			} else if (res >= 10 && res != 0)
+			{
+				printf(", ");
+			}*/
+			/*printf("%d", res);*/
+			/*if (i != 9)
+			{
+				if (res <= 9)
+				{
+					printf(",  ");
+				} else if (res >= 10)
+				{
+					printf(", ");
+				}
+			}*/
 			i++;
 		}
 		n++;
 		i = 0;
 		printf("\n");
 	}
-	/** while (i < 10)
-	{
-		int prod = 9 * i;
-		_putchar(prod + '0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar('\n');
-		i++;
-	}
+	/**
+	* while (i < 10)
+	* {
+	*	int prod = 9 * i;
+	*	_putcihar(prod + '0');
+	*	_putchar(',');
+	*	_putchar(' ');
+	*	_putchar(' ');
+	*	_putchar('\n');
+	*	i++;
+	*}
 	*/
 }
