f90fd38ec6226fa9c8b4bf89c2c1980e099ddf90
kiemaBruce <brucekiema292@gmail.com>
2023-03-24 09:47:51 +0000

    Draw a diagonal line on the terminal

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,10 +9,15 @@ int main(void)
 	* printf("%c: %d\n", a, _isupper(a));
 	*/
 	/* more_numbers();*/
-	print_line(0);
+	/*print_line(0);
 	print_line(2);
 	print_line(10);
 	print_line(-4);
+	*/
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
 
 	return(0);
 }
