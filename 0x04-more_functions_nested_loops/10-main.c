595a212ec614e9335b92d11b13384d3350c25eeb
kiemaBruce <brucekiema292@gmail.com>
2023-03-25 12:41:27 +0000

    Print a triangle, followed by a new line

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int main(void)
+{
+	/* print_numbers();*/
+	/* print_most_numbers(); */
+	/**
+	* char a = 'b';
+	* printf("%c: %d\n", a, _isupper(a));
+	*/
+	/* more_numbers();*/
+	/*print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	*/
+	/*
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	*/
+	/* print_square(2);
+	print_square(10);
+	print_square(0);
+	*/
+	/* fizz_buzz(); */
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+
+
+	return(0);
+}
