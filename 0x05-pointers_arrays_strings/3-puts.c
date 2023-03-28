2942941a4acbc5a25271de9152483f09aef029b8
kiemaBruce <brucekiema292@gmail.com>
2023-03-28 06:58:31 +0000

    Print a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -16,14 +16,20 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
+/**
+  * _strlen - returns the length of a string
+  * @s: the string whose length is to be determined
+  * Return: the string length as an integer
+  */
 int _strlen(char *s)
 {
 	int size, i;
+
 	size = 0;
 
-        for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		size++;
 	}
 	return (size);
- }
+}
