d1c53165b460f9ce57402573f2e19e2f307611f2
kiemaBruce <brucekiema292@gmail.com>
2023-03-29 13:10:58 +0000

    Correct code to be conformed to Betty style

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+  * print_array - prints the first n elements of an array of integers
+  * @a: the array of integers
+  * @n: the number of elements to be printed
+  */
 void print_array(int *a, int n)
 {
 	int i, length;
-	
+
 	length = 0;
 
 	while (a[length])
@@ -13,7 +18,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != (n-1))
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
