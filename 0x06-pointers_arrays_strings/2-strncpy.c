ca2acc4e5c3042ca2c8f13705c41d9a58225c12c
kiemaBruce <brucekiema292@gmail.com>
2023-03-30 07:43:06 +0000

    Correct code to conform to Betty style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+  * _strncpy - copies a string, including terminating null byte
+  * @dest: the destination buffer where the string is to be copied to
+  * @src: the source string to be copied to the destination buffer
+  * @n: the number of bytes from src to be copied to dest buffer
+  * Description: if n is greater than the length of src, the
+  * remaining spaces are filled with the null byte.
+  * Return: a pointer to dest string
+  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
