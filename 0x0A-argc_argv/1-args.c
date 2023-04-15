403ba57a46d18236be3370762b228d4ec60f2123
kiemaBruce <brucekiema292@gmail.com>
2023-04-15 08:26:37 +0000

    Print the number of arguments passed into the program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * main - prints the number of arguments passed into it
+  * main - prints the number of arguments passed into it.
   * @argc: a count of arguments supplied to the program
   * @argv: an array of pointers to the strings, which are those arguments
-  *
+  * Description: doesn't count the program name as an argument.
   * Return: EXIT_SUCCESS on success
   */
 int main(int argc, char __attribute__ ((unused)) *argv[])
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,4 +1,5 @@
 ## 0-whatsmyname.c
-Prints the program name followed by a new line.
+- Prints the program name followed by a new line.
 ## 1-args.c
-Prints the number of arguments passed into it, followed by a new line.
+- Prints the number of arguments passed into it, followed by a new line.
+- Doesn't count the program name as an argument.
