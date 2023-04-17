51cdf6d8007d0051b5454ed504f3a8b6ae05ced1
kiemaBruce <brucekiema292@gmail.com>
2023-04-17 21:07:09 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+## dog.h
+- Defines a new type struct dog with the following elements:
+	name, type = char *
+	age, type = float
+	owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+  * struct dog - simple structure for dog
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* dog_h */
