45bbbb42932612a7abb367ed4789fd783141167a
kiemaBruce <brucekiema292@gmail.com>
2023-04-19 20:19:39 +0000

    Take care of possible NULL pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,20 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, r;
 
-	if (array == NULL || cmp == NULL)
-	{
-		return (EXIT_FAILURE);
-	}
 	if (size <= 0)
 	{
 		return (-1);
 	}
+	if (array == NULL || cmp == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+	/*
+	*if (size <= 0)
+	*{
+	*	return (-1);
+	*}
+	*/
 	for (i = 0; i < size; i++)
 	{
 		r = (*cmp)(array[i]);
