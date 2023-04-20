86988ea1341e0432e24ed24f0d179059ecded528
kiemaBruce <brucekiema292@gmail.com>
2023-04-20 14:18:10 +0000

    Ensure code complies to all required compilation flags

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,13 +7,15 @@
   */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum, i, x;
+	unsigned int i;
+	int sum, x;
+
+	va_list args;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list args;
 
 	va_start(args, n);
 
