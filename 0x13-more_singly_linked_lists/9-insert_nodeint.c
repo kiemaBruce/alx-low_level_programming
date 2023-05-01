da04d3366a9fa10d3d29003b43c9e4334824a809
kiemaBruce <brucekiema292@gmail.com>
2023-05-01 11:48:56 +0000

    Handle malloc return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,11 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			new = malloc(sizeof(listint_t));
-			new->n = n;
 			if (new == NULL)
 			{
 				return (NULL);
 			}
+			new->n = n;
 			if (i == 0)
 			{
 				new->next = *head;
