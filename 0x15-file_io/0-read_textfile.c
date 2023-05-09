19184ebe98c6e3d28e3beaee6b81f01703d204ff
kiemaBruce <brucekiema292@gmail.com>
2023-05-09 01:14:28 +0000

    Adjust file opening mode to read and write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
+	/*fd = open(filename, O_RDONLY);*/
 	if (fd == -1)
 	{
 		return (0);
