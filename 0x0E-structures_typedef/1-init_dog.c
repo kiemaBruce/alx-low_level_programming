1aa105a082bae686304a75a32ce0c404784c53a1
kiemaBruce <brucekiema292@gmail..com>
2023-06-27 11:53:26 +0300

    Use stack memory instead of heap for the struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,58 +1,17 @@
 #include "dog.h"
 
 /**
-  * init_dog - initializes a variable of type struct dog
-  * @d: pointer to the struct dog
-  * @name: name of the dog
-  * @age: age of the dog
-  * @owner: dog owner
+  * init_dog - initializes a variable of type struct dog.
+  * @d: pointer to the variable of type struct dog.
+  * @name: the dog's name.
+  * @age: the dog's age.
+  * @owner: the dog's owner.
   */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int nlen, ownlen, i;
-
-	nlen = getLength(name);
-	ownlen = getLength(owner);
-
-	d->name = malloc(sizeof(char) * nlen);
-	if ((d->name) == NULL)
-	{
+	if (d == NULL)
 		return;
-	}
-	/* Copy name string to struct dog */
-	for (i = 0; i < nlen; i++)
-	{
-		(d->name)[i] = name[i];
-	}
-	d->owner = malloc(sizeof(char) * ownlen);
-	if (d->owner == NULL)
-	{
-		return;
-	}
-	/* Copy owner string to struct dog */
-	for (i = 0; i < ownlen; i++)
-	{
-		(d->owner)[i] = owner[i];
-	}
-
-	/*(*d).name = name;*/
+	d->name = name;
 	d->age = age;
-	/*(*d).owner = owner;*/
-}
-/**
-  * getLength - determines the length of a string
-  * @s: the string whose length is to be determined
-  * Return: the length as an int
-  */
-int getLength(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
+	d->owner = owner;
 }
