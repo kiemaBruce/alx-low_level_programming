fccdadba39b437d7ac72126cca5ac4377d93f25b
kiemaBruce <brucekiema292@gmail..com>
2023-07-07 11:53:57 +0300

    Produce correct output for more edge cases

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n);
 int _putchar(char c);
 unsigned long int _raiselong(int x, int exp);
 int get_bit(unsigned long int n, unsigned int index);
-char *binary_string(unsigned long int n);
+char *binary_string(unsigned long int n, int flag);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned long int binary_to_ulongint(const char *b);
