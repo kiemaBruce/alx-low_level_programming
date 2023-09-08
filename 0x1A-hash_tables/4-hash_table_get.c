4e36a661020173374b03740ed0d78fd9b125802e
kiemaBruce <brucekiema292@gmail.com>
2023-09-09 02:23:15 +0300

    Retrieve a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - retrieves a value associated with a key.
+  * @ht: pointer to the hash table where the value is to be retrieved
+  * from.
+  * @key: the key that is to be used to retrieve the value.
+  * Return: the value if it is found, otherwise return NULL.
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int hash_code;
+	unsigned long int index;
+	hash_node_t *temp;
+
+	hash_code = hash_djb2((const unsigned char *) key);
+	index = hash_code % ht->size;
+
+	if (ht->array[index] == NULL)
+	{/* Not found */
+		return (NULL);
+	}
+	if ((ht->array[index])->next == NULL)/* Just one node */
+	{
+		if (strcmp((ht->array[index])->key, (char *)key) == 0)
+			return ((ht->array[index])->value);
+		return (NULL);
+	}
+	/* Multiple nodes */
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, (char *)key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL); /* Not found */
+}
