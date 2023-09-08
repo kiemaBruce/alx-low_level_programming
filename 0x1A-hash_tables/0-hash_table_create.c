7a4c443ba6312d67b9da6d9198f8b7b31b6b7b4a
kiemaBruce <brucekiema292@gmail.com>
2023-09-09 01:40:34 +0300

    Return NULL when size is zero

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **arr;
 	unsigned long int *s, j;
 
+	if (size == 0)
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
