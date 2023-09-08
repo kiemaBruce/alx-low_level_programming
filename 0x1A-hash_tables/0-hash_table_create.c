a1099db4ae253496e19f8d26109492e9b2941b55
kiemaBruce <brucekiema292@gmail.com>
2023-09-09 01:41:29 +0300

    Revert changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **arr;
 	unsigned long int *s, j;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
