72feb611ecac0507009c187f48724b71f8a671ec
kiemaBruce <brucekiema292@gmail.com>
2023-11-03 11:20:23 +0300

    Initialize all hash table elements to NULL after creation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	hash_node_t **array;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
@@ -19,6 +20,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 	(*table).array = array;
 	return (table);
 }
