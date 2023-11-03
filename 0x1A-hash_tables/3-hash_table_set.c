e6a360ab543f0f125f62eacabf1ccb4f9638fe0f
kiemaBruce <brucekiema292@gmail.com>
2023-11-03 13:25:18 +0300

    Refine zero-length detection of key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (strlen(key) == 0)
+	if (strlen(key) == 0 or key[0] == '\0')
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
