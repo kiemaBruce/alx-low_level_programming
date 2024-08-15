9335c9ae6186022e000eb2db3ed74108d16b45fc
kiemaBruce <brucekiema292@gmail.com>
2024-08-15 17:41:26 +0300

    Search for an integer in an unsorted array using linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for an element in an array using linear search
+ * algorithm
+ * @array: pointer to the first element of the array to be searched.
+ * @size: the number of elements in the array.
+ * @value: the number to be searched for in the array.
+ * Return: The index of value in the array if it was found. If it wasn't found,
+ * or if array is NULL then the function returns -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int r, current_value;
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		current_value = *(array + i);
+		printf("Value checked array[%ld] = [%d]\n", i, current_value);
+		if (current_value == value)
+		{
+			r = current_value;
+			break;
+		}
+		if (i == size - 1)
+			r = -1;
+	}
+	return (r);
+}
